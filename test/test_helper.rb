151fec5dbae4dc2b50082ceec39bd7dbea620917
beco-ippei <beco.ippei@gmail.com>
2016-01-24 01:42:48 +0900

    mod coverall settings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,7 @@
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'docomo_api'
 
 require 'minitest/autorun'
-
-if ENV['CI']
-  require 'coveralls'
-  Coveralls.wear!
-
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[Coveralls::SimpleCov::Formatter]
-  SimpleCov.start 'test_frameworks'
-end
