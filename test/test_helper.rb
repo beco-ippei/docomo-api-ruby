31c44fbbb58aa4359ce7d2a3a59d0f730826d4de
beco-ippei <beco.ippei@gmail.com>
2016-01-24 01:35:52 +0900

    add coverall

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,11 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'docomo_api'
 
 require 'minitest/autorun'
+
+if ENV['CI']
+  require 'coveralls'
+  Coveralls.wear!
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[Coveralls::SimpleCov::Formatter]
+  SimpleCov.start 'test_frameworks'
+end
